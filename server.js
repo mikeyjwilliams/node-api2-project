50b8620688c2e62eae74e4f28d31668feabb75c8
Mikey Williams <manicdesigner@gmail.com>
2020-02-14 23:58:47 -0500

    postsRouter added to server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
 const server = express();
+const postsRouter = require('./routes/posts');
 
 server.use(express.json());
+
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
 	res.send('posts is up');
