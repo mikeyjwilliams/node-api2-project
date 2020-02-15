09325a2e232d5cfd2dec2cc22b439e92455e1be8
Michael Williams <manicdesigner@gmail.com>
2020-02-15 12:35:50 -0500

    post comments

--- routes/comments.js
+++ routes/comments.js
@@ -2,9 +2,34 @@ const express = require('express');
 const db = require('../data/db');
 const router = express.Router({ mergeParams: true });
 
+router.post('/', (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+
+  if (!text) {
+    return res
+      .status(400)
+      .json({ message: 'Please provide text for the comment.' });
+  }
+  const comment = {
+    post_id: id,
+    text: text,
+  };
+  db.insertComment(comment)
+    .then(newComment => {
+      res.status(201).json(newComment);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: 'There was an error while saving the comment to the database',
+      });
+    });
+});
+
 router.get('/', (req, res) => {
   const { id } = req.params;
-  db.findCommentById(id)
+  db.findPostComments(id)
     .then(comment => {
       if (comment) {
         res.status(200).json(comment);
